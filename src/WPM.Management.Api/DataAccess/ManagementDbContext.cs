d941830ee61cc9cf3904fda71f7b00169e99db45
vikesh-bansal <vikesh.bansal1@gmail.com>
2025-09-25 22:02:03 +0530

    added snoopy new Pet

--- src/WPM.Management.Api/DataAccess/ManagementDbContext.cs
+++ src/WPM.Management.Api/DataAccess/ManagementDbContext.cs
@@ -21,7 +21,8 @@ namespace WPM.Management.Api.DataAccess
                 [
                     new Pet(){Id = 1, Name="Gianni", Age=13, BreedId=1},
                     new Pet(){Id=2, Name="Nina", Age=10, BreedId=1},
-                    new Pet(){Id=3, Name="Cati", Age=12, BreedId=2}
+                    new Pet(){Id=3, Name="Cati", Age=12, BreedId=2},
+                    new Pet(){Id=4, Name="Snoopy", Age=15, BreedId=1}
                 ]
 
                 );
